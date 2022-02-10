9864b5e049673bc82c73552fbbbc5d023ff1bd9b
James <jamestariga@live.com>
2022-02-10 12:35:44 -0800

    Edit space.js

--- src/components/Space.js
+++ src/components/Space.js
@@ -8,11 +8,11 @@ const Space = (data) => {
     <>
       <h3>{title}</h3>
       <p>Date of Capture: {date}</p>
-      {media_type == 'image' &&
+      {media_type === 'image' &&
         <div className="image">
           <a href={url}><img src={url} alt={url} /></a>
         </div>}
-      {media_type == 'video' &&
+      {media_type === 'video' &&
         <div className="image">
           <iframe src={url} title={title} />
         </div>}
