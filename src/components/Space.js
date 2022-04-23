39e5451e074bb50fdebe8452627de1165bcf1798
James <jamestariga08@live.com>
2022-04-22 23:11:54 -0700

    ✨ Updated the content design adding a hover effect and changing the overall layout

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,22 +1,29 @@
 import React from 'react'
 
 const Space = (data) => {
-
   const { title, date, explanation, url, media_type } = data
 
   return (
     <>
-      <h3>{title}</h3>
-      <p>Date of Capture: {date}</p>
-      {media_type === 'image' &&
-        <div className="image">
-          <a href={url}><img src={url} alt={url} /></a>
-        </div>}
-      {media_type === 'video' &&
-        <div className="image">
+      {media_type === 'image' && (
+        <div className='image'>
+          <a href={url}>
+            <img src={url} alt={url} />
+          </a>
+        </div>
+      )}
+      {media_type === 'video' && (
+        <div className='video'>
           <iframe src={url} title={title} />
-        </div>}
-      <p>{explanation}</p>
+        </div>
+      )}
+      <div className='content-header'>
+        <h3>{title}</h3>
+        <p>Date of Capture: {date}</p>
+      </div>
+      <div className='paragraph'>
+        <p>{explanation}</p>
+      </div>
     </>
   )
 }
